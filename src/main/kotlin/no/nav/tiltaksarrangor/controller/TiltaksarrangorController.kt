1e1d940035690caaea024379f6cb21f0445e63f9
Tia Firing <tia.firing@nav.no>
2023-03-21 15:49:54 +0100

    logger for testing

--- src/main/kotlin/no/nav/tiltaksarrangor/controller/TiltaksarrangorController.kt
+++ src/main/kotlin/no/nav/tiltaksarrangor/controller/TiltaksarrangorController.kt
@@ -3,6 +3,7 @@ package no.nav.tiltaksarrangor.controller
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.tiltaksarrangor.client.AmtTiltakClient
 import no.nav.tiltaksarrangor.utils.Issuer
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -12,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController
 class TiltaksarrangorController(
 	private val amtTiltakClient: AmtTiltakClient
 ) {
+	private val log = LoggerFactory.getLogger(javaClass)
+
 	@GetMapping("/meg/roller")
 	@ProtectedWithClaims(issuer = Issuer.TOKEN_X)
 	fun getMineRoller(): List<String> {
+		log.info("Mottok kall for å hente roller")
 		return amtTiltakClient.getMineRoller()
 	}
 }
