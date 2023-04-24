3c4f4f1bf835a525e453760aee3a7246f2ac0d4c
Izis Frøyhov <izis.froyhov@nav.no>
2023-04-24 08:59:59 +0200

    Fjerner gammel gjennomforing status

--- src/main/kotlin/no/nav/tiltaksarrangor/koordinator/model/Deltakerliste.kt
+++ src/main/kotlin/no/nav/tiltaksarrangor/koordinator/model/Deltakerliste.kt
@@ -15,6 +15,6 @@ data class Deltakerliste(
 	val deltakere: List<Deltaker>
 ) {
 	enum class Status {
-		APENT_FOR_INNSOK, IKKE_STARTET, GJENNOMFORES, AVSLUTTET
+		APENT_FOR_INNSOK, GJENNOMFORES, AVSLUTTET
 	}
 }
