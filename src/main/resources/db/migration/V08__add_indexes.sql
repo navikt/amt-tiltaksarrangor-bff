c34d3e8ae2858f260142f73f3e195f1d5217ec44
Tia Firing <tia.firing@nav.no>
2023-06-26 13:37:14 +0200

    oppretter indexene concurrently

--- src/main/resources/db/migration/V08__add_indexes.sql
+++ src/main/resources/db/migration/V08__add_indexes.sql
@@ -1,4 +1,4 @@
-CREATE INDEX IF NOT EXISTS veileder_deltaker_deltaker_id_idx ON veileder_deltaker (deltaker_id);
-CREATE INDEX IF NOT EXISTS koordinator_deltakerliste_deltakerliste_id_idx ON koordinator_deltakerliste (deltakerliste_id);
-CREATE INDEX IF NOT EXISTS deltaker_deltakerliste_id_idx ON deltaker (deltakerliste_id);
-CREATE INDEX IF NOT EXISTS endringsmelding_deltaker_id_idx ON endringsmelding (deltaker_id);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS veileder_deltaker_deltaker_id_idx ON veileder_deltaker (deltaker_id);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS koordinator_deltakerliste_deltakerliste_id_idx ON koordinator_deltakerliste (deltakerliste_id);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS deltaker_deltakerliste_id_idx ON deltaker (deltakerliste_id);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS endringsmelding_deltaker_id_idx ON endringsmelding (deltaker_id);
