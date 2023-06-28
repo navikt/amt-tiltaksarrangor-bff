11f6b9bee105aa8d614fe45429aa5a576f261f93
Tia Firing <tia.firing@nav.no>
2023-06-28 15:48:12 +0200

    prøver uten cache

--- src/main/kotlin/no/nav/tiltaksarrangor/client/amtarrangor/AmtArrangorClient.kt
+++ src/main/kotlin/no/nav/tiltaksarrangor/client/amtarrangor/AmtArrangorClient.kt
@@ -1,9 +1,7 @@
 package no.nav.tiltaksarrangor.client.amtarrangor
 
-import com.github.benmanes.caffeine.cache.Caffeine
 import no.nav.tiltaksarrangor.ingest.model.AnsattDto
 import no.nav.tiltaksarrangor.model.exceptions.UnauthorizedException
-import no.nav.tiltaksarrangor.utils.CacheUtils
 import no.nav.tiltaksarrangor.utils.JsonUtils
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
@@ -12,7 +10,6 @@ import okhttp3.RequestBody.Companion.toRequestBody
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
-import java.time.Duration
 import java.util.UUID
 
 @Component
@@ -23,33 +20,27 @@ class AmtArrangorClient(
 	private val log = LoggerFactory.getLogger(javaClass)
 	private val mediaTypeJson = "application/json".toMediaType()
 
-	private val personidentToAnsattCache = Caffeine.newBuilder()
-		.expireAfterWrite(Duration.ofHours(1))
-		.build<String, AnsattDto>()
-
 	fun getAnsatt(personIdent: String): AnsattDto? {
-		return CacheUtils.tryCacheFirstNullable(personidentToAnsattCache, personIdent) {
-			val request = Request.Builder()
-				.url("$amtArrangorUrl/api/ansatt")
-				.get()
-				.build()
+		val request = Request.Builder()
+			.url("$amtArrangorUrl/api/ansatt")
+			.get()
+			.build()
 
-			amtArrangorHttpClient.newCall(request).execute().use { response ->
-				if (!response.isSuccessful) {
-					when (response.code) {
-						401 -> throw UnauthorizedException("Ikke tilgang til å hente ansatt")
-						403 -> throw UnauthorizedException("Ikke tilgang til å hente ansatt")
-						404 -> return@tryCacheFirstNullable null
-						else -> {
-							log.error("Kunne ikke hente ansatt fra amt-arrangør, responsekode: ${response.code}")
-							throw RuntimeException("Kunne ikke hente ansatt")
-						}
+		amtArrangorHttpClient.newCall(request).execute().use { response ->
+			if (!response.isSuccessful) {
+				when (response.code) {
+					401 -> throw UnauthorizedException("Ikke tilgang til å hente ansatt")
+					403 -> throw UnauthorizedException("Ikke tilgang til å hente ansatt")
+					404 -> return null
+					else -> {
+						log.error("Kunne ikke hente ansatt fra amt-arrangør, responsekode: ${response.code}")
+						throw RuntimeException("Kunne ikke hente ansatt")
 					}
 				}
-				val body = response.body?.string() ?: throw RuntimeException("Tom responsbody")
-
-				return@tryCacheFirstNullable JsonUtils.fromJsonString<AnsattDto>(body)
 			}
+			val body = response.body?.string() ?: throw RuntimeException("Tom responsbody")
+
+			return JsonUtils.fromJsonString<AnsattDto>(body)
 		}
 	}
 
