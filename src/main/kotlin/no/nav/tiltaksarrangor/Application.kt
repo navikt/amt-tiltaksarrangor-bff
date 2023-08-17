48e47eecfe25bcbf51945060509d5f3f3207d66f
Tormod Fossum <110398791+tofoss@users.noreply.github.com>
2023-08-17 09:09:29 +0200

    Legg til retrymekanisme ved deadlocks i upsert av ansatt (#106)
    * Legg til retrymekanisme ved deadlocks i upsert av ansatt
    
    * Fjern delay ved konsumering av ansatte
    
    * Legg til magiske aspekter for å få appen til å kjøre
    
    * Bruk kafkaimage for arm prosessorer ved behov

--- src/main/kotlin/no/nav/tiltaksarrangor/Application.kt
+++ src/main/kotlin/no/nav/tiltaksarrangor/Application.kt
@@ -3,11 +3,13 @@ package no.nav.tiltaksarrangor
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.retry.annotation.EnableRetry
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableJwtTokenValidation
 @EnableScheduling
+@EnableRetry
 class Application
 
 fun main(args: Array<String>) {
