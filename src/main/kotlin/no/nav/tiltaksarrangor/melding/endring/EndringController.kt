501ea15e890b3ea1e5f62394ed41e054776d34e9
Tormod Fossum <tormod.fossum@nav.no>
2024-08-07 09:52:05 +0200

    Legg til inputvalidering av endringer

--- src/main/kotlin/no/nav/tiltaksarrangor/melding/endring/EndringController.kt
+++ src/main/kotlin/no/nav/tiltaksarrangor/melding/endring/EndringController.kt
@@ -4,6 +4,7 @@ import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.tiltaksarrangor.melding.MeldingTilgangskontrollService
 import no.nav.tiltaksarrangor.melding.endring.request.EndringFraArrangorRequest
 import no.nav.tiltaksarrangor.melding.endring.request.LeggTilOppstartsdatoRequest
+import no.nav.tiltaksarrangor.repositories.model.DeltakerlisteDbo
 import no.nav.tiltaksarrangor.utils.Issuer
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -27,6 +28,17 @@ class EndringController(
 
 	private fun opprettEndring(deltakerId: UUID, request: EndringFraArrangorRequest) =
 		tilgangskontrollService.medTilgangTilAnsattOgDeltaker(deltakerId) { ansatt, deltaker, deltakerliste ->
+			valider(request, deltakerliste)
 			endringService.endreDeltaker(deltaker, deltakerliste, ansatt, request)
 		}
+
+	private fun valider(request: EndringFraArrangorRequest, deltakerliste: DeltakerlisteDbo) {
+		when (request) {
+			is LeggTilOppstartsdatoRequest -> validerOppstartsdato(
+				request.startdato,
+				request.sluttdato,
+				deltakerliste,
+			)
+		}
+	}
 }
