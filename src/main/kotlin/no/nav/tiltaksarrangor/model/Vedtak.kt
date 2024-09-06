7b0fb0b41726c65c2e36986a9b3131df4a8b2178
Line Moseng <line.moseng@nav.no>
2024-09-06 10:34:10 +0200

    Egen status for vedtak i historikk

--- src/main/kotlin/no/nav/tiltaksarrangor/model/Vedtak.kt
+++ src/main/kotlin/no/nav/tiltaksarrangor/model/Vedtak.kt
@@ -28,5 +28,54 @@ data class DeltakerVedVedtak(
 	val deltakelsesprosent: Float?,
 	val bakgrunnsinformasjon: String?,
 	val deltakelsesinnhold: Deltakelsesinnhold?,
-	val status: DeltakerStatus,
+	val status: HistorikkStatus,
 )
+
+data class HistorikkStatus(
+	val id: UUID,
+	val type: Type,
+	val aarsak: Aarsak?,
+	val gyldigFra: LocalDateTime,
+	val gyldigTil: LocalDateTime?,
+	val opprettet: LocalDateTime,
+) {
+	data class Aarsak(
+		val type: Type,
+		val beskrivelse: String?,
+	) {
+		init {
+			if (beskrivelse != null && type != Type.ANNET) {
+				error("Aarsak $type skal ikke ha beskrivelse")
+			}
+		}
+
+		enum class Type {
+			SYK,
+			FATT_JOBB,
+			TRENGER_ANNEN_STOTTE,
+			FIKK_IKKE_PLASS,
+			IKKE_MOTT,
+			ANNET,
+			AVLYST_KONTRAKT,
+			UTDANNING,
+			SAMARBEIDET_MED_ARRANGOREN_ER_AVBRUTT,
+		}
+	}
+
+	enum class Type {
+		KLADD,
+		UTKAST_TIL_PAMELDING,
+		AVBRUTT_UTKAST,
+		VENTER_PA_OPPSTART,
+		DELTAR,
+		HAR_SLUTTET,
+		IKKE_AKTUELL,
+		FEILREGISTRERT,
+		SOKT_INN,
+		VURDERES,
+		VENTELISTE,
+		AVBRUTT,
+		FULLFORT,
+		PABEGYNT_REGISTRERING,
+	}
+}
