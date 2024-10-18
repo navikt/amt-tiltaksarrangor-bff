ca3e3fd23e56cb4470dde5ce5d0cd217ddb8041a
Tia Firing <tia.firing@nav.no>
2024-10-18 06:45:15 +0200

    endrer toggle så vi alltid er master for AFT

--- src/main/kotlin/no/nav/tiltaksarrangor/melding/MeldingTilgangskontrollService.kt
+++ src/main/kotlin/no/nav/tiltaksarrangor/melding/MeldingTilgangskontrollService.kt
@@ -31,7 +31,7 @@ class MeldingTilgangskontrollService(
 			?.takeIf { it.deltakerliste.erTilgjengeligForArrangor() }
 			?: throw NoSuchElementException("Fant ikke deltaker med id $deltakerId")
 
-		if (!unleashService.erForslagSkruddPa(deltakerMedDeltakerliste.deltakerliste.tiltakType)) {
+		if (!unleashService.erKometMasterForTiltakstype(deltakerMedDeltakerliste.deltakerliste.tiltakType)) {
 			throw UnauthorizedException("Endepunkt er utilgjenglig")
 		}
 
