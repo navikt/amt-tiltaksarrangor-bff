f3cbd44a5584f623bb490c2a1ea4924db0298316
Marthe Frogner Holm <marthe@Mac.localdomain>
2025-02-19 14:22:47 +0100

    test å flytte ut data calss

--- src/main/kotlin/no/nav/tiltaksarrangor/controller/response/UlestEndringResponse.kt
+++ src/main/kotlin/no/nav/tiltaksarrangor/controller/response/UlestEndringResponse.kt
@@ -19,15 +19,15 @@ data class UlestEndringResponse(
 	JsonSubTypes.Type(value = DeltakerEndringResponse::class, name = "DeltakelsesEndring"),
 	JsonSubTypes.Type(value = ForslagHistorikkResponse::class, name = "AvvistForslag"),
 )
-sealed interface OppdateringResponse {
-	data class DeltakelsesEndringResponse(
-		val endring: DeltakerEndringResponse,
-	) : OppdateringResponse
+sealed interface OppdateringResponse
 
-	data class AvvistForslagResponse(
-		val forslag: ForslagHistorikkResponse,
-	) : OppdateringResponse
-}
+data class DeltakelsesEndringResponse(
+	val endring: DeltakerEndringResponse,
+) : OppdateringResponse
+
+data class AvvistForslagResponse(
+	val forslag: ForslagHistorikkResponse,
+) : OppdateringResponse
 
 fun List<UlestEndring>.toResponse(
 	ansatte: Map<UUID, NavAnsatt>,
@@ -38,14 +38,14 @@ fun List<UlestEndring>.toResponse(
 		is Oppdatering.DeltakelsesEndring -> UlestEndringResponse(
 			it.id,
 			it.deltakerId,
-			OppdateringResponse.DeltakelsesEndringResponse(
+			DeltakelsesEndringResponse(
 				it.oppdatering.endring.toResponse(ansatte, enheter, arrangornavn),
 			),
 		)
 		is Oppdatering.AvvistForslag -> UlestEndringResponse(
 			it.id,
 			it.deltakerId,
-			OppdateringResponse.AvvistForslagResponse(
+			AvvistForslagResponse(
 				it.oppdatering.forslag.toResponse(arrangornavn, ansatte, enheter),
 			),
 		)
