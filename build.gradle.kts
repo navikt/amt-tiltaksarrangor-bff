6c24e914cdb1766a5f1983565d01ebf815e0c2c9
Tormod Fossum <tormod.fossum@nav.no>
2025-05-19 14:03:02 +0200

    Bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    val kotlinVersion = "2.1.20"
+    val kotlinVersion = "2.1.21"
 
     id("org.springframework.boot") version "3.4.5"
     id("io.spring.dependency-management") version "1.1.7"
@@ -21,7 +21,7 @@ repositories {
 
 val logstashEncoderVersion = "8.1"
 val kafkaClientsVersion = "4.0.0"
-val tokenSupportVersion = "5.0.25"
+val tokenSupportVersion = "5.0.27"
 val okHttpVersion = "4.12.0"
 val kotestVersion = "5.9.1"
 val testcontainersVersion = "1.21.0"
