7a48e3d2cdc1275efa7a1030429cfac037f63b8e
MartheFH <marthe.frogner.holm@nav.no>
2025-06-16 09:42:01 +0200

    Fjern endepunkter og service for enrginsmelding mot frontend

--- src/main/kotlin/no/nav/tiltaksarrangor/endringsmelding/api/response/EndringsmeldingResponse.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.tiltaksarrangor.endringsmelding.api.response
-
-import no.nav.tiltaksarrangor.model.Endringsmelding
-
-data class EndringsmeldingResponse(
-	val aktiveEndringsmeldinger: List<Endringsmelding>,
-	val historiskeEndringsmeldinger: List<Endringsmelding>,
-)
