85afe0b5837a074ab71b64c676fe4ce9716dd628
Roar Skinderviken <roar.skinderviken@gmail.com>
2025-06-27 14:19:13 +0200

    Removed redundant @EnableKafka. Made dataSource private.

--- src/test/kotlin/no/nav/tiltaksarrangor/RepositoryTestBase.kt
+++ src/test/kotlin/no/nav/tiltaksarrangor/RepositoryTestBase.kt
@@ -7,16 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureJdbc
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
-import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.test.context.ActiveProfiles
 import javax.sql.DataSource
 
 @ActiveProfiles("test")
 @AutoConfigureJdbc
-@EnableKafka
 abstract class RepositoryTestBase {
 	@Autowired
-	protected lateinit var dataSource: DataSource
+	private lateinit var dataSource: DataSource
 
 	@Autowired
 	protected lateinit var template: NamedParameterJdbcTemplate
@@ -27,6 +25,6 @@ abstract class RepositoryTestBase {
 	companion object {
 		@ServiceConnection
 		@Suppress("unused")
-		val container = postgresContainer
+		private val container = postgresContainer
 	}
 }
